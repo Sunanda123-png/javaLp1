a6af5b42c22829dfcc630e01ccd4395fa5128452
Sunanda123-png <sunanda02@gmail.com>
2021-08-08 23:22:42 +0530

    adding only src

--- src/com/sunanda/EmpWages.java
+++ src/com/sunanda/EmpWages.java
@@ -2,7 +2,7 @@ package com.sunanda;
 
 public class EmpWages {
     public static void main(String[] args)
-    {
+    /*{
         int IS_FULL_TIME = 1;
         double empCheck = Math.floor(Math.random()*10)%2;
         if (empCheck==IS_FULL_TIME)
@@ -13,5 +13,8 @@ public class EmpWages {
         {
             System.out.println("Employee is not present");
         }
+    }*/
+    {
+        
     }
 }
