6016afda481c6d5a47bd1441c930f4810a12e047
Sunanda123-png <sunanda02@gmail.com>
2021-08-09 21:20:48 +0530

    calculate the wages

--- src/com/sunanda/EmpWages.java
+++ src/com/sunanda/EmpWages.java
@@ -1,20 +1,14 @@
 package com.sunanda;
 
-public class EmpWages {
-    public static void main(String[] args)
-    /*{
-        int IS_FULL_TIME = 1;
-        double empCheck = Math.floor(Math.random()*10)%2;
-        if (empCheck==IS_FULL_TIME)
-        {
-            System.out.println("Employee is present");
-        }
-        else
-        {
-            System.out.println("Employee is not present");
-        }
-    }*/
-    {
+import java.lang.invoke.SwitchPoint;
 
+public class EmpWages {
+    public static void main(String[] args){
+
+        int wages=20;
+        int hour=8;
+        int c;
+        c=wages*hour;
+        System.out.println("The wages of the employee is = "+c);
     }
 }
