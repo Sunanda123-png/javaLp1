dc81bfbf3bfe24f892adc77068aa78e5c3711829
Sunanda123-png <sunanda02@gmail.com>
2021-08-09 22:45:25 +0530

    Calculating wages for month

--- src/com/sunanda/EmpWages.java
+++ src/com/sunanda/EmpWages.java
@@ -8,21 +8,28 @@ public class EmpWages {
         System.out.println("The hour is 8 and wages is 20");
         int full_time_employee=1;
         int part_time_employee=0;
+        int i;
+        int work_hrs;
+        int result=0;
         int emp;
         int emp_hrs=0;
-        emp=(int)(Math.random()*10)%2;
-        switch(emp)
-        {
-            case 1:
-                System.out.println("Employee is full time ");
-                emp_hrs=8;
-                break;
-            case 0:
-                System.out.println("Employee is part time ");
-                emp_hrs=4;
-                break;
+        for(i=0;i<=20;i++) {
+            emp = (int) (Math.random() * 10) % 2;
+            switch (emp) {
+                case 1:
+                    work_hrs=(int)(Math.random() * 10) % 9;
+                    result=result+work_hrs;
+                    System.out.println("Work time "+work_hrs);
+                    break;
+                case 0:
+                    work_hrs=(int)(Math.random() * 10) % 9;
+                    result=result+work_hrs;
+                    System.out.println("Work time "+work_hrs);
+                    break;
+            }
         }
-        int total_wages= emp_hrs * 20;
+        System.out.println("Total working hrs = "+result);
+        int total_wages= result * 20;
         System.out.println("The wages of the employee is = "+total_wages);
     }
 }
