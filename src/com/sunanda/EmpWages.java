45e22be48f79eccc54d1e94dbc51473a8bbc4ef2
Sunanda123-png <sunanda02@gmail.com>
2021-08-09 21:46:43 +0530

    Adding part time employee

--- src/com/sunanda/EmpWages.java
+++ src/com/sunanda/EmpWages.java
@@ -3,12 +3,26 @@ package com.sunanda;
 import java.lang.invoke.SwitchPoint;
 
 public class EmpWages {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int wages=20;
-        int hour=8;
-        int c;
-        c=wages*hour;
-        System.out.println("The wages of the employee is = "+c);
+        System.out.println("The hour is 8 and wages is 20");
+        int full_time_employee=1;
+        int part_time_employee=0;
+        int emp;
+        int emp_hrs=0;
+        emp=(int)(Math.random()*10)%2;
+        switch(emp)
+        {
+            case 1:
+                System.out.println("Employee is full time ");
+                emp_hrs=8;
+                break;
+            case 0:
+                System.out.println("Employee is part time ");
+                emp_hrs=4;
+                break;
+        }
+        int total_wages= emp_hrs * 20;
+        System.out.println("The wages of the employee is = "+total_wages);
     }
 }
